ce7082ce27c5cbcb90e0d37d992213a6d3ab26ad
Ahmed Mahdy <mo.a9tay@gmail.com>
2023-09-19 19:56:41 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/coursat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coursat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:coursat/theme/custom_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: CustomTheme.darkTheme(context),
+      home: const Scaffold(),
+    );
+  }
+}
